83816f561a9a4e80f2dc4d49ce59a068667c5b9d
dileep2508 <128730226+dileep2508@users.noreply.github.com>
2025-02-24 23:34:50 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,15 @@
+package CollegeClass;
+
+public class SumArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,4,5,6,7};
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum +=arr[i];
+		}
+		System.out.println("Sum Number is : "+sum);
+	}
+
+}
