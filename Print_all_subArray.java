c421b2061aa796f30557ff94136ff341b94ed482
dileep2508 <128730226+dileep2508@users.noreply.github.com>
2025-02-27 11:59:32 +0530

    Add files via upload

--- Print_all_subArray.java
+++ Print_all_subArray.java
@@ -4,8 +4,10 @@ public class Print_all_subArray {
 
     public static void main(String[] args) {
         int sum = 0;
-        int[] arr = {1, 2, 3, 4}; 
+        int[] arr = {1, 2, 3, 4,5}; 
         printAllSubArrays(arr, sum);
+        maxSum(arr);
+        
     }
 
     public static void printAllSubArrays(int[] arr, int sum) {
@@ -22,6 +24,19 @@ public class Print_all_subArray {
                 }
                 System.out.println("] -> Sum: "+ sum);
             }
+            
         }
+    }
+    public static void maxSum(int arr[]) {
+    	int max_sum = 1 ;
+    	int currSum = 0;
+    	for(int i=0; i<arr.length; i++) {
+    		currSum += arr[i];
+    		max_sum = Math.max(max_sum,currSum);
+    		if(currSum < 0) {
+    			currSum = 0;
+    		}
+    	}
+    	System.out.print("MaxSum : "+max_sum);
     }
 } 
