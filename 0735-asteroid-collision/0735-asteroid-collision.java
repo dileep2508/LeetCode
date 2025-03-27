b91d12a9419c630b72e61a4a9b3a628556a3b712
dileep2508 <128730226+dileep2508@users.noreply.github.com>
2025-03-27 10:51:37 +0530

    Time: 6 ms (67.97%), Space: 45.3 MB (85.94%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> s = new Stack<>();
-        for(int asteroid : asteroids){
+        for(int asteroid : asteroids ){
             boolean ans = true;
-            while(!s.isEmpty() && asteroid < 0 && s.peek() > 0){
+            while(!s.isEmpty() && asteroid < 0 && s.peek()>0){
                 if(s.peek() < -asteroid){
                     s.pop();
                     continue;
@@ -11,18 +11,20 @@ class Solution {
                 else if(s.peek() == -asteroid){
                     s.pop();
                 }
-                ans = false;
-                break;
+                ans =false;
+                break;  
             }
             if(ans){
-                s.push(asteroid);
+                    s.push(asteroid);
             }
-        }
-        int[] res = new int[s.size()];
+            }
+
+            int[] res = new int [s.size()];
             for(int i=s.size()-1; i>=0; i--){
                 res[i] = s.pop();
             }
             return res;
-    } 
+        }
+    
     
 }
