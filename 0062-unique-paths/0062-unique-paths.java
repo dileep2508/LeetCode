0d5963ebeea52613b7fb5e049408e25d8c536068
dileep2508 <128730226+dileep2508@users.noreply.github.com>
2025-04-18 15:48:25 +0530

    Time: 0 ms (100%), Space: 40.7 MB (26.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,8 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       
+       if(m==12 || n==1){
+        return 1;
+       }
         int[][] dp = new int[m][n];
 
         for(int i=0; i<m; i++){
